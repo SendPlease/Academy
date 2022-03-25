ea83d4c48eacbf9e8c9c177fcb844a7ab7e40cea
Send <84020118+SendPlease@users.noreply.github.com>
2022-03-25 15:07:22 +0100

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,18 @@
+if (tree[block.father].code I= "*) {
+block.code = tree[block.father].code+
++1°:
+}
+else{
+if (block.letter
+== tree[minIndex].letter){
+block.code
+"0°•
+}
+else if (block.letter == tree[preminIndex].letter){
+block.code
+"1°
+7
+else {
+FindCode(tree[block.father]);
+block.code = tree[block.father].code
+"0°:
